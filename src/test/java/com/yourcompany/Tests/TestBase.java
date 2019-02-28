60ba9f110a98ceb036745bc26348f8dc690e17ac
laszlosimity <laszlosimity@SL-1242.local>
2019-02-28 11:00:00 +0000

    Changing to EU DU

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -103,7 +103,7 @@ public class TestBase  {
 
         // Launch remote browser and set it as the current thread
         webDriver.set(new RemoteWebDriver(
-                new URL("https://" + username + ":" + accesskey + "@ondemand.saucelabs.com:443/wd/hub"),
+                new URL("https://" + username + ":" + accesskey + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub"),
                 capabilities));
 
         // set current sessionId
